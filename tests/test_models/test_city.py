d861b45113f4e6c61b5b9e8feaf6558cf388d328
jfbm74 <jfbm74@gmail.com>
2020-07-01 17:09:22 -0500

    fixing test city  class

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -56,3 +56,72 @@ class TestCityDocs(unittest.TestCase):
                              "{:s} method failed in docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method failed in docstring".format(func[0]))
+
+
+class TestCity(unittest.TestCase):
+    """TestCity: to test City Class"""
+    def test_save_Method(self):
+        """ Function: test_saveMethod
+                      to test save instance method
+        """
+        x = City()
+        x.save()
+        self.assertNotEqual(x.created_at, x.updated_at)
+
+    def test_to_dict_Method(self):
+        """ Function: test_to_dictMethod
+                      to test to_dict instance method
+        """
+        y = City()
+        CityDict = y.to_dict()
+        self.assertEqual(y.__class__.__name__, 'City')
+        self.assertIsInstance(CityDict['created_at'], str)
+        self.assertIsInstance(CityDict['updated_at'], str)
+        self.assertIsInstance(CityDict['id'], str)
+
+    def test_has_methods(self):
+        """ Function: test_attribs
+                      to test if have all methods available
+        """
+        w = City()
+        self.assertTrue(hasattr(w, "__init__"))
+        self.assertTrue(hasattr(w, "__str__"))
+        self.assertTrue(hasattr(w, "save"))
+        self.assertTrue(hasattr(w, "to_dict"))
+
+    def test_hasatribs(self):
+        """ Function: test_hasaattribs
+                      to test if have all basics attribs available
+        """
+        z = City()
+        self.assertTrue(hasattr(z, "name"))
+        self.assertTrue(hasattr(z, "state_id"))
+        self.assertTrue("id" in z.__dict__)
+        self.assertTrue("created_at" in z.__dict__)
+        self.assertTrue("updated_at" in z.__dict__)
+
+    def test_init(self):
+        """ Function: test_init
+                      to test CityClass
+        """
+        w = City()
+        self.assertTrue(isinstance(w, City))
+        self.assertIsInstance(w, City)
+
+    def test_str(self):
+        z = City()
+        stringA = str(z)
+        stringB = z.__str__()
+        self.assertTrue(stringA, stringB)
+
+    def test_types(self):
+        i = City()
+        self.assertEqual(type(i.name), str)
+        self.assertEqual(type(i.state_id), str)
+
+    def test_inherit(self):
+        j = City()
+        self.assertTrue(issubclass(j.__class__, BaseModel), True)
+
+if __name__ == "__main__":
+    unittest.main()
